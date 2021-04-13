7cefee791a4103d3b5e40f9aa6524ac2ceabc5d7
Dmitri Pisarev <dimaip@gmail.com>
2021-04-13 15:11:49 +0300

    Init

---
+++ index.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const url = require('url');
+const atob = require('atob');
+const {PDFDocument, rgb} = require('pdf-lib');
+const fetch = require('node-fetch');
+const fontkit = require('@pdf-lib/fontkit');
+const md5 = require('md5');
+
+const blue = rgb(86 / 255, 107 / 255, 255 / 255)
+
+const config = {
+  hostname: 'localhost',
+  port: 8088
+}
+
+fetch('https://github.com/googlefonts/opensans/raw/main/fonts/ttf/OpenSans-Regular.ttf').then((res) => res.arrayBuffer()).then(openSansBytes => {
+
+  const server = http.createServer(async (req, res) => {
+    const queryObject = url.parse(req.url, true).query;
+
+
+    const signature = JSON.parse(decodeURIComponent(atob(queryObject.data)));
+
+    if (!signature.url.startsWith("https://psmb-neos-resources.hb.bizmrg.com/") && !signature.url.startsWith("https://sfi.ru/")) {
+      throw new Error('Invalid URL')
+    }
+
+    const existingPdfBytes = await fetch(signature.url).then(res => res.arrayBuffer())
+
+    const pdfDoc = await PDFDocument.load(existingPdfBytes)
+    pdfDoc.registerFontkit(fontkit);
+    const openSansFont = await pdfDoc.embedFont(openSansBytes);
+
+    const pages = pdfDoc.getPages()
+    const firstPage = pages[0]
+    const {height} = firstPage.getSize()
+
+    firstPage.drawRectangle({
+      x: 25,
+      y: height - 125,
+      width: 320,
+      height: 100,
+      borderWidth: 2,
+      borderColor: blue,
+    })
+    const title = 'Документ подписан простой электронной подписью\nДата и время подписания: ' + signature.signDate + '\nФИО подписавшего документ: ' + signature.signee + '\nДолжность: ' + signature.signeePosition + '\nУникальный программный ключ:\n' + md5(signature.signee)
+
+    firstPage.drawText(title, {size: 10, font: openSansFont, x: 30, y: height - 40, color: blue, lineHeight: 15})
+    const pdfBytes = await pdfDoc.save()
+
+    res.statusCode = 200;
+    res.contentType = "application/pdf"
+    res.end(Buffer.from(pdfBytes))
+  });
+
+  server.listen(config.port, config.hostname, () => {
+    console.info(`Server running at http://${config.hostname}:${config.port}/`);
+  });
+});
